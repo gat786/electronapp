16c697b9788c932109c3c2d8137b01c8de3ea0c4
gat786 <ganesht049@gmail.com>
2019-06-22 11:19:14 +0530

    js changed

--- main.js
+++ main.js
@@ -19,9 +19,9 @@ function createWindow () {
 
 
 
-  // win.webContents.openDevTools();
+   win.webContents.openDevTools();
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('./html/index.html')
 
   // Emitted when the window is closed.
   win.on('closed', () => {
